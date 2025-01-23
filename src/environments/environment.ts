fa5383dde49439ec791654f605a56393db60e78c
Siva2357 <sivakurra.ksp2357@gmail.com>
2025-01-23 15:53:15 +0530

    removed public key now can be made public

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,7 @@
 export const environment = {
     production: false,
     firebaseConfig : {
-      apiKey: "AIzaSyBvQGA0Mf-fj_6c9VYz4AFuDl3yjD9DHas",
-      authDomain: "file---upload.firebaseapp.com",
-      projectId: "file---upload",
-      storageBucket: "file---upload.firebasestorage.app",
-      messagingSenderId: "472298543771",
-      appId: "1:472298543771:web:71ce1e85c8dd8d86fe843e",
-      measurementId: "G-GM8C00ERVP"
+
     }
   };
   
